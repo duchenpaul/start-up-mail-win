081b98b9288d93511e37d3878873eaad5f3464f0
Chenny Du <duchenpaul@gmail.com>
2018-09-08 14:09:33 +0800

    upgrade compile script

--- compile.bat
+++ compile.bat
@@ -1,2 +1,9 @@
+SET appName=text2keystroke
 pyinstaller --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico --noconsole
-pause
+move dist\%appName%.exe %cd%
+
+del /s /f /q  text2keystroke.spec
+
+REM walkaround RD bug
+timeout 1
+RD /S /Q   dist\ build\
