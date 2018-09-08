b7f11c79ffd266f6b72a361dc74bce5e833cc4f1
Chenny Du <duchenpaul@gmail.com>
2018-09-08 14:46:20 +0800

    Change upx path to local

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 SET appName=start_up_mail_win
-pyinstaller --upx-dir C:\Users\chdu\Desktop\upx-3.95-win64 --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico --noconsole
-
+pyinstaller --upx-dir upx --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico --noconsole
+rem pyinstaller --upx-dir upx --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico
 move dist\%appName%.exe %cd%
 
 del /s /f /q  start_up_mail_win.spec
