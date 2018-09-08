9b247a59222323fde68ec6b36a44341e457dd020
chdu <duchenpaul@gmail.com>
2018-09-08 14:26:44 +0800

    Add UPX
    Add UPX to reduce the app size

--- compile.bat
+++ compile.bat
@@ -1,8 +1,9 @@
-SET appName=text2keystroke
-pyinstaller --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico --noconsole
+SET appName=start_up_mail_win
+pyinstaller --upx-dir C:\Users\chdu\Desktop\upx-3.95-win64 --onefile  start_up_mail_win.py --icon Babasse-Old-School-Mail.ico --noconsole
+
 move dist\%appName%.exe %cd%
 
-del /s /f /q  text2keystroke.spec
+del /s /f /q  start_up_mail_win.spec
 
 REM walkaround RD bug
 timeout 1
