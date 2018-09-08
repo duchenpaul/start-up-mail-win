18754ccea1541cb34535a38c729d0a00faabe5f4
chdu <duchenpaul@gmail.com>
2018-09-08 13:41:55 +0800

    Extend camera focus wait time

--- cam_cap_pygame.py
+++ cam_cap_pygame.py
@@ -7,7 +7,7 @@ def cam_cap():
     cam = pygame.camera.Camera(0,(640,480))
     cam.start()
     img = cam.get_image()
-    time.sleep(1)
+    time.sleep(3)
     img = cam.get_image()
     img_path = '{}\\cam_{}.jpg'.format(tempfile.gettempdir(),time.strftime('%Y%m%d_%H%M%S'))
     pygame.image.save(img,img_path)
